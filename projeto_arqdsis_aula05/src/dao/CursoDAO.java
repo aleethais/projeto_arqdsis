91d8ab21645c2d5d4e4c95bb1dfe7853314ce2a5
Alessandro e Thais <aleethais>
2016-07-21 04:34:53 -0300

    Aula_05

---
+++ projeto_arqdsis_aula05/src/dao/CursoDAO.java
@@ -0,0 +1,89 @@
+package dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import factory.ConnectionFactory;
+//import java.util.ArrayList;
+import to.CursoTO;
+
+public class CursoDAO {
+	
+	public void incluir(CursoTO to) {
+		String sqlInsert = "INSERT INTO curso(id, nome,dataInicio,dataTermino,hora,vagas,valor) VALUES (?,?,?,?,?,?,?)";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+			PreparedStatement st = conn.prepareStatement(sqlInsert);){
+			 st.setInt(1,to.getId());
+	         st.setString(2,to.getNome());
+	         st.setString(3,to.getDataInicio());
+	         st.setString(4,to.getDataTermino());
+	         st.setString(5,to.getHora());
+	         st.setInt(6,to.getVagas());
+	         st.setDouble(7,to.getValor());
+	         st.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void atualizar(CursoTO to) {
+		String sqlUpdate = "UPDATE curso SET nome = ?, dataInicio = ?, dataTermino = ?, hora = ?, vagas = ?, valor = ? WHERE id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+			PreparedStatement st = conn.prepareStatement(sqlUpdate);){
+			
+	         st.setString(1,to.getNome());
+	         st.setString(2,to.getDataInicio());
+	         st.setString(3,to.getDataTermino());
+	         st.setString(4,to.getHora());
+	         st.setInt(5,to.getVagas());
+	         st.setDouble(6,to.getValor());	
+	         st.setInt(7,to.getId());
+	         st.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void excluir(CursoTO to) {
+		String sqlDelete = "DELETE FROM curso WHERE id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+			PreparedStatement st = conn.prepareStatement(sqlDelete);) {
+			st.setInt(1, to.getId());
+			st.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public CursoTO carregar(int id) {
+		CursoTO to = new CursoTO();
+		String sqlSelect = "SELECT id,nome,dataInicio,dataTermino,hora,vagas,valor FROM curso WHERE curso.id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement st = conn.prepareStatement(sqlSelect);) {
+			st.setInt(1, id);
+			try (ResultSet rs = st.executeQuery();) {
+				if (rs.next()) {
+					to.setId(rs.getInt("id"));
+					to.setNome(rs.getString("nome"));
+					to.setDataInicio(rs.getString("DataInicio"));
+					to.setDataTermino(rs.getString("dataTermino"));
+					to.setHora(rs.getString("hora"));
+					to.setVagas(rs.getInt("vagas"));
+					to.setValor(rs.getDouble("Valor"));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return to;
+	}
+
+}
---
+++ projeto_arqdsis_aula05/src/insercao_de_valores.sql
@@ -0,0 +1,4 @@
+-- Inserção de Valores na Tabela Aluno e Curso para rodar os testes
+
+INSERT into aluno(id,nome,endereco,telefone,email,rg,cpf) values(1,'Alessandro','Rua Sousa','1111-1111','alessandro@gmail.com','111111111','11111111111');
+INSERT into curso(id,nome,dataInicio,dataTermino,hora,vagas,valor) values(1,'Sistemas de Informação','2016-02-02','2017-02-28','22:00', 220, 300.00);
