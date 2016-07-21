9cd7c59b523aa940dcc70080bc50406d1ea7652d
Alessandro e Thais <aleethais>
2016-07-21 02:21:53 -0300

    Aula_05

---
+++ projeto_arqdsis_aula05/src/controller/ListarCursosController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Atendente;
+import to.AlunoTO;
+
+/**
+ * Servlet implementation class ListarClientesController
+ */
+@WebServlet("/listar_cursos.do")
+public class ListarCursosController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String chave = request.getParameter("data[search]");
+			Atendente atendente = new Atendente();
+			ArrayList<AlunoTO> lista;
+			if(chave != null && chave.length() > 0){
+				lista = atendente.listarCursos(chave);
+			} else {
+				lista = atendente.listarCursos();
+			}
+			request.setAttribute("lista", lista);
+		RequestDispatcher dispatcher = request.
+				getRequestDispatcher("ListarCursos.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ projeto_arqdsis_aula05/src/model/Atendente.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.ArrayList;
+
+import dao.AtendenteDAO;
+import to.AlunoTO;
+import to.CursoTO;
+
+public class Atendente {
+	public ArrayList<AlunoTO> listarAlunos(){
+		ArrayList<AlunoTO> lista;
+		AtendenteDAO dao = new AtendenteDAO();
+		lista = dao.listarAlunos();
+		return lista;
+	}
+	public ArrayList<AlunoTO> listarAlunos(String chave){
+		ArrayList<AlunoTO> lista;
+		AtendenteDAO dao = new AtendenteDAO();
+		lista = dao.listarAlunos(chave);
+		return lista;
+	}
+	public ArrayList<CursoTO> listarCursos(){
+		ArrayList<CursoTO> lista;
+		AtendenteDAO dao = new AtendenteDAO();
+		lista = dao.listarCursos();
+		return lista;
+	}
+	public ArrayList<CursoTO> listarCursos(String chave){
+		ArrayList<CursoTO> lista;
+		AtendenteDAO dao = new AtendenteDAO();
+		lista = dao.listarCursos(chave);
+		return lista;
+	}
+
+}
